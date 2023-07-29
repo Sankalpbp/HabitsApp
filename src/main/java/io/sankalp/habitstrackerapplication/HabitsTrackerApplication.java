2c9bd9eaf9836f839fea4541c137fa5ef9774447
Sankalpbp <sankalpnitj@gmail.com>
2023-07-30 02:20:02 +0530

    Initial commit - setting up the package structure

---
+++ src/main/java/io/sankalp/habitstrackerapplication/HabitsTrackerApplication.java
@@ -0,0 +1,13 @@
+package io.sankalp.habitstrackerapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitsTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabitsTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/sankalp/habitstrackerapplication/HabitsTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.sankalp.habitstrackerapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitsTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
